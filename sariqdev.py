7c390259a962fd216ecfb0c072bcc65147e880bb
Irmukhammedov <irmukhammedovanvar@gmail.com>
2023-08-30 11:58:48 +0500

    Update file sariqdev

--- sariqdev.py
+++ sariqdev.py
@@ -273,3 +273,17 @@ Mening darslarim
 #     print(
 #         f"{ism} {tyil}-yilda {tjoy}da tavallud topgan. " f"{vyil-tyil} yil umr ko'rgan."
 #     )
+
+
+# kinolar = {
+#     "ali": ["Terminator", "Rambo", "Titanic"],
+#     "vali": ["Tenet", "Inception", "Interstellar"],
+#     "hasan": ["Abdullajon", "Bomba", "Shaytanat"],
+#     "husan": ["Mahallada duv-duv gap", "John Wick"],
+# }
+
+
+# for ism, kinolar in kinolar.items():
+#     print(f"\n{ism.title()} ning sevimli kinolari:")
+#     for kino in kinolar:
+#         print(kino)
